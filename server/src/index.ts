c4c1205ecb6be8b08de8f4acd2c005ac5ebf64e4
Dor <dorpele10@gmail.com>
2021-02-04 18:04:22 +0200

    added mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import mongoose from 'mongoose';
 import { createServer } from 'http';
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
@@ -10,6 +11,12 @@ const app = express();
 
 const http = createServer(app);
 
-http.listen(port, () => {
+http.listen(port, async () => {
   console.log(`Server is listening on ${process.env.HOST_URL}:${port}`);
+
+  await mongoose.connect(
+    'mongodb://localhost:27017/socket-io-chat?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+  console.log('Connected to MongoDB!');
 });
